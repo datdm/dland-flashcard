cda723cbae9e49a0aef189921497eedac20b9123
datdm <datdm.work@gmail.com>
2026-05-15 01:45:38 +0900

    Update build

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ import path from "path";
 const nextConfig: NextConfig = {
   // Exclude backend packages from Next.js bundling
   // outputFileTracingRoot: path.join(__dirname, "./"),
-  outputFileTracingExcludes: {
-    "*": ['./server/**/*', './server']
-  },
+  // outputFileTracingExcludes: {
+  //   "*": ['./server/**/*', './server']
+  // },
   experimental: {
     serverComponentsExternalPackages: ['pg', 'bcrypt']
   }
