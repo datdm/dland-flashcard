0663320871b942a50c8678790d118e3bce28b513
datdm <datdm.work@gmail.com>
2026-05-15 00:11:20 +0900

    Update build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['pg', 'bcrypt']
+  },
   webpack: (config) => {
     config.externals.push('./server')
     return config
   },
+  turbopack: {
+    
+  }
 };
 
 export default nextConfig;
