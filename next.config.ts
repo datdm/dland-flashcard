c5f19cd59957e6b3852dfa0ea1c08e9d45e281ba
datdm <datdm.work@gmail.com>
2026-05-15 00:16:14 +0900

    Update build

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Exclude server folder from Next.js compilation
   experimental: {
-    serverComponentsExternalPackages: ['pg', 'bcrypt']
+    serverComponentsExternalPackages: ['pg', 'bcrypt'],
   },
   webpack: (config) => {
     config.externals.push('./server')
     return config
   },
   turbopack: {
-    
-  }
+    // Empty config to acknowledge Turbopack usage
+    // The server folder is already external and won't be bundled
+  },
 };
 
 export default nextConfig;
